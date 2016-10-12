e2bb45f76a6eb974d32404a8aec02cbf5f072ff0
Juan Cubeddu <juancubeddu@Juans-MacBook-Pro-2.local>
2016-10-12 11:04:08 -0500

    new objects

--- data-constants.js
+++ data-constants.js
@@ -1,3 +1,6 @@
 module.exports = {
   titleBlock: { "data-block-field": "title","data-type": "string", "data-format-type", "string_textfield" },
+  descBlock: {"data-block-field": "description", "data-type": "text_long", "data-form-type": "text_textarea", "data-format-type": "text_default"},
+  imageBlock: { "data-block-field": "name", "data-type": "image", "data-form-type": "image_image", "data-format-type": "image" },
+  linkBlock : { "data-block-field": "name", "data-type": "link", "data-form-type": "link_default", "data-format-type": "link" },
 };
